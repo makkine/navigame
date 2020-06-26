fe786ab9f71b6b28eec8038d75fce83677d18c83
makkine <smakkine@gmail.com>
2020-06-25 19:40:29 -0500

    First commit
    Boat and stuff

---
+++ Assets/Coastline.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.XR.WSA;
+using System.CodeDom.Compiler;
+
+public class Coastline : MonoBehaviour
+{
+    public boat boat;
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeText();
+        if (displayInfo)
+        {
+            if (Input.GetKeyDown("x"))
+            {
+                boat.riding = false;
+            }
+
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        if (boat.riding){
+            displayInfo = true;
+
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText()
+
+    {
+        if (displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+
+        }
+
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/boat.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.XR.WSA;
+using System.CodeDom.Compiler;
+
+public class boat : MonoBehaviour
+{
+    public PController rider;
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    public bool riding;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeText();
+        if (riding)
+        {
+            Vector3 tempTransform = rider.transform.position;
+            tempTransform.y = -1.3f;
+            Quaternion tempRot = rider.transform.rotation;
+            this.transform.rotation = tempRot;
+            this.transform.position = tempTransform;
+        }
+        if (displayInfo)
+        {
+            if (Input.GetKeyDown("x"))
+            {
+                rider.transform.position = this.transform.position;
+                riding = true;
+                displayInfo = false;
+            }
+
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        displayInfo = true;
+    }
+
+    void OnTriggerExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText()
+
+    {
+        if (displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+
+        }
+
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+
+    }
+}
