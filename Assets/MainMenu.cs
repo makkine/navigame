f1c64f44a4d8aa9b210652f1dfb7173ac9f6cd97
Dom DiCarlo <ddicarlo@uchicago.edu>
2020-07-01 23:59:08 -0500

    tricky working with git and unity.. adding in some options menu UI thing for development

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LoadOpenWater()
+    {
+        SceneManager.LoadScene("Openocean");
+        SceneManager.UnloadScene("Menu");
+    }
+
+    public void LoadMainland()
+    {
+        SceneManager.LoadScene("Mainland");
+        SceneManager.UnloadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
